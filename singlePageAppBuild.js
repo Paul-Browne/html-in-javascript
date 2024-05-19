36195b6f4e3d23fbc9e445fe18fe31e55d31ce75
Paul Browne <paul.browne@elisa.fi>
2024-05-19 21:26:50 +0300

    tidy

--- singlePageAppBuild.js
+++ singlePageAppBuild.js
@@ -31,7 +31,7 @@ const pageShell = async preload => {
             head(
                 meta({ charset: "UTF-8" }),
                 meta({ name: "viewport", content: "width=device-width, initial-scale=1.0" }),
-                link({ rel:"stylesheet", href:"/css/style.css" }),
+                link({ rel: "stylesheet", href:"/css/style.css" }),
                 link({ rel: 'modulepreload', href:preload, crossOrigin:true }),
                 script(inlinedRouter),
                 zone('head'),
--- src/js/updateZones.js
+++ src/js/updateZones.js
@@ -1,17 +1,17 @@
 export default (key, ...content) => {
     var iterator = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_COMMENT);
-    var curNode;
-    while (curNode = iterator.nextNode()) {
-        if(!curNode.nodeValue.indexOf(key)){
-            while(curNode.nextSibling){
-                if(curNode.nextSibling.nodeType === Node.COMMENT_NODE && !curNode.nextSibling.nodeValue.indexOf(`/${key}`)){
-                    const temp = document.createElement('i')    // doesn't matter what element
-                    curNode.after(temp)
-                    temp.outerHTML = content.join('');
+    var currentNode;
+    while (currentNode = iterator.nextNode()) {
+        if(!currentNode.nodeValue.indexOf(key)){
+            while(currentNode.nextSibling){
+                if(currentNode.nextSibling.nodeType === Node.COMMENT_NODE && !currentNode.nextSibling.nodeValue.indexOf(`/${key}`)){
+                    const tempNode = document.createElement('i')    // doesn't matter what element
+                    currentNode.after(tempNode)
+                    tempNode.outerHTML = content.join('');
                     window.preloader?.refreshLinkObserver()
                     return
                 }
-                curNode.nextSibling.remove()
+                currentNode.nextSibling.remove()
             }
         }     
     }
